a593385a1c3f554cf4915a1708b33847dd520193
Mihara3389 <m_mihara@emyus.jp>
2021-06-02 23:12:51 +0900

    テスト画面実装

---
+++ StrutsAutoSystem/src/main/java/dao/QuestionDao.java
@@ -0,0 +1,34 @@
+package dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.QuestionBean;
+
+public class QuestionDao {
+    private Connection conn;
+    private PreparedStatement ps  = null;
+    private ResultSet rs  = null;
+    
+    public  List<QuestionBean> getQuestion() throws SQLException {
+        //dao.ConnectionDaoからデータベースへの接続を得る
+        conn = ConnectionDao.getConnect();
+        //SQLステートメント、オブジェクトの作成(users)
+        String sql = "select * from questions";
+        ps = conn.prepareStatement(sql);
+        //結果の出力
+        rs = ps.executeQuery();
+        //結果を格納
+        List<QuestionBean> questionlist = new ArrayList<QuestionBean>();
+        while (rs.next()) {        	
+        	QuestionBean q = new QuestionBean();
+        	q.setId(rs.getInt("id"));
+        	q.setQuestion(rs.getString("question"));
+        	questionlist.add(q);
+        }
+        return questionlist;
+    }
+}
